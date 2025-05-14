644edcc01b87c39f0132900625ea4b30c7116ebb
Jonathan P. Peixoto 喬尼·佩肖托 <jonypeixotooriginal@gmail.com>
2025-05-14 10:21:22 -0300

    Add files via upload

---
+++ src/enums/user-agent.ts
@@ -0,0 +1,9 @@
+export enum UserAgentProfile {
+    WINDOWS_EDGE = 'windows-edge',
+    MACOS_SAFARI = 'macos-safari',
+    SMART_TV_GENERIC = 'smarttv-generic',
+    SMART_TV_TIZEN = 'smarttv-tizen',
+    VR_OCULUS = 'vr-oculus',
+    DEFAULT = 'default',
+    CUSTOM = 'custom',
+}
